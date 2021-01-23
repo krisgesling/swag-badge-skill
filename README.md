329f7c3031bd042765a4817cc106ad8a7841000b
Kris Gesling <kris.gesling@mycroft.ai>
2021-01-23 06:46:52 +0000

    hello world

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # <img src="https://raw.githack.com/FortAwesome/Font-Awesome/master/svgs/solid/id-badge.svg" card_color="#22A7F0" width="50" height="50" style="vertical-align:bottom"/> Swag Badge
-
+Let Mycroft join you at LCA
 
 ## About
-
-
-## Examples
+Anything your Mycroft instance speaks will be published to your LinuxConfAu Swag Badge (or other MQTT compatible device).
 
 ## Credits
 krisgesling
--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+from subprocess import call, Popen
 from mycroft import MycroftSkill, intent_file_handler
 
 
@@ -5,9 +6,28 @@ class SwagBadge(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('badge.swag.intent')
-    def handle_badge_swag(self, message):
-        self.speak_dialog('badge.swag')
+    def initialize(self):
+        self.add_event('speak', self.log_to_badge)
+
+    def log_to_badge(self, message):
+        speech = message.data['utterance']
+        mqtt_host = self.settings.get("mqtt_host")
+        badge_id = self.settings.get("badge_id")
+        mqtt_topic = f"public/{badge_id}/0/in"
+        cmd = [
+               "mosquitto_pub",
+               "-h",
+               mqtt_host,
+               "-t",
+               mqtt_topic,
+               "-m",
+               f"(oled:log {speech})"
+        ]
+        if mqtt_host and badge_id:
+            call(cmd)
+        else:
+            self.log.info("Could not publish speech to Swag Badge")
+            self.log.info("Please check your Skill settings at https://home.mycroft.ai")
 
 
 def create_skill():
